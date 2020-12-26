4177a8dea5d06f8e2fb50f17e7088624c1f8b5fc
mohammed ismail <mohammedismailck123@gmail.com>
2020-12-26 14:44:23 +0530

    added hostel images in student page

--- student/hostels.php
+++ student/hostels.php
@@ -3,7 +3,7 @@
 <div class="container">
     <div class="card-deck cards-format ">
       <div class="card border-secondary">
-        <img class="card-img-top" src="../web/images/nitc.jpg" alt="Card image cap">
+        <img class="card-img-top" src="assets/images/a-hostel.jpg" alt="Card image cap">
         <div class="card-body">
           <h5 class="card-title">A Hostel</h5>
           <p class="card-text">For 1st year students</p>
@@ -11,18 +11,18 @@
         </div>
       </div>
       <div class="card border-secondary">
-        <img class="card-img-top" src="../web/images/nitc.jpg" alt="Card image cap">
+        <img class="card-img-top card-image" src="assets/images/b-hostel.jpg" alt="Card image cap">
         <div class="card-body">
           <h5 class="card-title">B Hostel</h5>
-          <p class="card-text">For 1st year students</p>
+          <p class="card-text">For 2nd year students</p>
           <a href="application-form.php?id=B" class="btn btn-primary">Apply</a>
         </div>
       </div>
       <div class="card border-secondary">
-        <img class="card-img-top" src="../web/images/nitc.jpg" alt="Card image cap">
+        <img class="card-img-top card-image" src="assets/images/c-hostel.jpg" alt="Card image cap">
         <div class="card-body">
           <h5 class="card-title">C Hostel</h5>
-          <p class="card-text">For 1st year students</p>
+          <p class="card-text">For 3rd year students</p>
           <a href="application-form.php?id=C" class="btn btn-primary">Apply</a>
         </div>
       </div>
@@ -34,26 +34,26 @@
 <div class="container second-card">
     <div class="card-deck cards-format">
       <div class="card border-secondary">
-        <img class="card-img-top" src="../web/images/nitc.jpg" alt="Card image cap">
+        <img class="card-img-top" src="assets/images/d-hostel.jpg" alt="Card image cap">
         <div class="card-body">
         <h5 class="card-title">D Hostel</h5>
-          <p class="card-text">For 1st year students</p>
+          <p class="card-text">For 3rd year students</p>
           <a href="application-form.php?id=D" class="btn btn-primary">Apply</a>
         </div>
       </div>
       <div class="card border-secondary">
-        <img class="card-img-top" src="../web/images/nitc.jpg" alt="Card image cap">
+        <img class="card-img-top" src="assets/images/e-hostel.jpg" alt="Card image cap">
         <div class="card-body">
         <h5 class="card-title">E Hostel</h5>
-          <p class="card-text">For 1st year students</p>
+          <p class="card-text">For 4th year students</p>
           <a href="application-form.php?id=E" class="btn btn-primary">Apply</a>
         </div>
       </div>
       <div class="card border-secondary">
-        <img class="card-img-top" src="../web/images/nitc.jpg" alt="Card image cap">
+        <img class="card-img-top" src="assets/images/f-hostel.jpg" alt="Card image cap">
         <div class="card-body">
         <h5 class="card-title">F Hostel</h5>
-          <p class="card-text">For 1st year students</p>
+          <p class="card-text">For 4th year students</p>
           <a href="application-form.php?id=F" class="btn btn-primary">Apply</a>
         </div>
       </div>
