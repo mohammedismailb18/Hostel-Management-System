151d485fc92d82065ae059d149a9a48ef00515a9
Muhammed Shifan <muhammedshifanpazheri@gmail.com>
2020-12-26 10:37:36 +0530

    Database ready

---
+++ hostel_management.sql
@@ -0,0 +1,257 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 25, 2020 at 05:23 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hostel_management`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `administrator`
+--
+
+CREATE TABLE `administrator` (
+  `ha_id` int(10) NOT NULL,
+  `f_name` varchar(20) DEFAULT NULL,
+  `l_name` varchar(20) DEFAULT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  `mobile` varchar(20) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `application`
+--
+
+CREATE TABLE `application` (
+  `application_no` int(10) NOT NULL,
+  `student_id` varchar(20) DEFAULT NULL,
+  `room_id` int(20) DEFAULT NULL,
+  `hm_id` int(20) DEFAULT NULL,
+  `message` varchar(1000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hostel`
+--
+
+CREATE TABLE `hostel` (
+  `hostel_id` int(10) NOT NULL,
+  `hostel_name` varchar(20) DEFAULT NULL,
+  `no_of_rooms` int(5) DEFAULT NULL,
+  `no_of_students` int(5) DEFAULT NULL,
+  `accepted_gen` int(2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hostel_manager`
+--
+
+CREATE TABLE `hostel_manager` (
+  `hm_id` int(10) NOT NULL,
+  `f_name` varchar(20) DEFAULT NULL,
+  `l_name` varchar(20) DEFAULT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  `mobile` varchar(20) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  `admin` int(10) DEFAULT NULL,
+  `hostel_id` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `msg_id` int(10) NOT NULL,
+  `hm_id` int(10) DEFAULT NULL,
+  `student_id` varchar(20) DEFAULT NULL,
+  `subject` varchar(100) DEFAULT NULL,
+  `message` varchar(1000) DEFAULT NULL,
+  `time_stamp` datetime DEFAULT NULL,
+  `sender` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mobile_number`
+--
+
+CREATE TABLE `mobile_number` (
+  `student_id` varchar(20) NOT NULL,
+  `mobile` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room`
+--
+
+CREATE TABLE `room` (
+  `room_id` int(10) NOT NULL,
+  `room_no` int(5) DEFAULT NULL,
+  `hostel_id` int(10) DEFAULT NULL,
+  `allocated` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `student_id` varchar(20) NOT NULL,
+  `f_name` varchar(20) DEFAULT NULL,
+  `l_name` varchar(20) DEFAULT NULL,
+  `year` varchar(20) DEFAULT NULL,
+  `dept` varchar(50) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  `hostel_id` int(20) DEFAULT NULL,
+  `room_id` int(10) DEFAULT NULL,
+  `gender` int(2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `administrator`
+--
+ALTER TABLE `administrator`
+  ADD PRIMARY KEY (`ha_id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `mobile` (`mobile`);
+
+--
+-- Indexes for table `application`
+--
+ALTER TABLE `application`
+  ADD PRIMARY KEY (`application_no`),
+  ADD KEY `fk_application_student_id` (`student_id`),
+  ADD KEY `fk_application_room_id` (`room_id`),
+  ADD KEY `fk_application_hm_id` (`hm_id`);
+
+--
+-- Indexes for table `hostel`
+--
+ALTER TABLE `hostel`
+  ADD PRIMARY KEY (`hostel_id`);
+
+--
+-- Indexes for table `hostel_manager`
+--
+ALTER TABLE `hostel_manager`
+  ADD PRIMARY KEY (`hm_id`),
+  ADD UNIQUE KEY `mobile` (`mobile`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD KEY `fk_hostel_manager_admin` (`admin`),
+  ADD KEY `fk_hostel_manager_hostel_id` (`hostel_id`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`msg_id`),
+  ADD KEY `fk_messages_hm_id` (`hm_id`),
+  ADD KEY `fk_messages_student_id` (`student_id`);
+
+--
+-- Indexes for table `mobile_number`
+--
+ALTER TABLE `mobile_number`
+  ADD PRIMARY KEY (`student_id`,`mobile`),
+  ADD UNIQUE KEY `mobile` (`mobile`);
+
+--
+-- Indexes for table `room`
+--
+ALTER TABLE `room`
+  ADD PRIMARY KEY (`room_id`),
+  ADD KEY `fk_room_hostel_id` (`hostel_id`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`student_id`),
+  ADD KEY `fk_student_hostel_id` (`hostel_id`),
+  ADD KEY `fk_student_room_id` (`room_id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `application`
+--
+ALTER TABLE `application`
+  ADD CONSTRAINT `fk_application_hm_id` FOREIGN KEY (`hm_id`) REFERENCES `hostel_manager` (`hm_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_application_room_id` FOREIGN KEY (`room_id`) REFERENCES `room` (`room_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_application_student_id` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `hostel_manager`
+--
+ALTER TABLE `hostel_manager`
+  ADD CONSTRAINT `fk_hostel_manager_admin` FOREIGN KEY (`admin`) REFERENCES `administrator` (`ha_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_hostel_manager_hostel_id` FOREIGN KEY (`hostel_id`) REFERENCES `hostel` (`hostel_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `messages`
+--
+ALTER TABLE `messages`
+  ADD CONSTRAINT `fk_messages_hm_id` FOREIGN KEY (`hm_id`) REFERENCES `hostel_manager` (`hm_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_messages_student_id` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `mobile_number`
+--
+ALTER TABLE `mobile_number`
+  ADD CONSTRAINT `fk_mobile_number_student_id` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `room`
+--
+ALTER TABLE `room`
+  ADD CONSTRAINT `fk_room_hostel_id` FOREIGN KEY (`hostel_id`) REFERENCES `hostel` (`hostel_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `student`
+--
+ALTER TABLE `student`
+  ADD CONSTRAINT `fk_student_hostel_id` FOREIGN KEY (`hostel_id`) REFERENCES `hostel` (`hostel_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_student_room_id` FOREIGN KEY (`room_id`) REFERENCES `room` (`room_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
