ecf47a103316b359c3782c4ded7e81fb0f11cbc6
Muhammed Shifan P <76591730+shifanpazheri@users.noreply.github.com>
2020-12-26 21:26:14 +0530

    login and signup ready

---
+++ index_ha.php
@@ -0,0 +1,58 @@
+
+<!DOCTYPE html>
+
+<head>
+    <title>HMS - Hostel Admin</title>
+    <!-- Meta tag Keywords -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8" />
+    <meta name="keywords"
+        content="Flat lay login form Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+    <!-- //Meta tag Keywords -->
+
+    <link href="web/css/font.css" rel="stylesheet">
+
+    <!--/Style-CSS -->
+    <link rel="stylesheet" href="web/css/style.css" type="text/css" media="all" />
+    <!--//Style-CSS -->
+
+    <script src="web/w3.js" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+    <!-------------- Login Form ----------------------------------------------->
+    <section class="w3l-workinghny-form">
+        <!-- /form -->
+        <div class="workinghny-form-grid">
+            <div class="wrapper">
+                <div class="logo">
+                    <h1><a class="brand-logo" href="index.html">Hostel Management System</a></h1>
+                    <!-- if logo is image enable this
+                        <a class="brand-logo" href="#index.html">
+                            <img src="image-path" alt="Your logo" title="Your logo" style="height:35px;" />
+                        </a> -->
+                </div>
+                <div class="workinghny-block-grid">
+                    <div class="form-right-inf">
+                        <h2>Hostel Admin Login</h2>
+                        <div class="login-form-content">
+                            <form action="#" class="signin-form" method="post">
+                                <div class="one-frm">
+                                    <input type="text" name="Ha_id" placeholder="Hostel Admin id" required="" autofocus>
+                                </div>
+                                <div class="one-frm">
+                                    <input type="password" name="Password" placeholder="Password" required="">
+                                </div>
+                                <button class="btn btn-style mt-3">Login </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-------------------------------------------------------- -->
+</body>
+
+</html>
---
+++ index_signup.php
@@ -0,0 +1,83 @@
+
+<!DOCTYPE html>
+
+<head>
+    <title>HMS - Student SignUp</title>
+    <!-- Meta tag Keywords -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8" />
+    <meta name="keywords"
+        content="Flat lay login form Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+    <!-- //Meta tag Keywords -->
+
+    <link href="web/css/font.css" rel="stylesheet">
+
+    <!--/Style-CSS -->
+    <link rel="stylesheet" href="web/css/style.css" type="text/css" media="all" />
+    <!--//Style-CSS -->
+
+    <script src="web/w3.js" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+    <!-------------- Login Form ----------------------------------------------->
+    <section class="w3l-workinghny-form">
+        <!-- /form -->
+        <div class="workinghny-form-grid">
+            <div class="wrapper">
+                <div class="logo">
+                    <h1><a class="brand-logo" href="index.html">Hostel Management System</a></h1>
+                    <!-- if logo is image enable this
+                        <a class="brand-logo" href="#index.html">
+                            <img src="image-path" alt="Your logo" title="Your logo" style="height:35px;" />
+                        </a> -->
+                </div>
+                <div class="workinghny-block-grid">
+                    <div class="form-right-inf">
+                        <h2>Student SignUp</h2>
+                        <div class="login-form-content">
+                            <form action="#" class="signin-form" method="post">
+                                <div class="one-frm">
+                                    <input type="text" name="student_id" placeholder=" Student Roll No" required="" autofocus>
+                                </div>
+                                <div class="one-frm">
+                                    <input type="text" name="f_name" placeholder=" First Name" required="" autofocus>
+                                </div>
+                                <div class="one-frm">
+                                    <input type="text" name="l_name" placeholder=" Last Name" required="" autofocus>
+                                </div>
+                                <div class="one-frm">
+                                    <input type="text" name="year" placeholder=" Year" required="" autofocus>
+                                </div>
+                                <div class="one-frm">
+                                    <input type="text" name="dept" placeholder=" Department" required="" autofocus>
+                                </div>
+                                <div class="one-frm">
+                                    <input type="password" name="Password" placeholder="Password" required="">
+                                </div>
+                                <div class="one-frm">
+                                    <input type="password" name="Password_conf" placeholder="Confirm your Password" required="">
+                                </div>
+                          <!--      <div class="one-frm">
+                                    Male:<input type="radio button" name="gender[]">
+                                </div>
+                                <div class="one-frm">
+                                    Female:<input type="radio button" name="gender[]">
+                                </div>
+                                <div class="one-frm">
+                                    Others:<input type="radio button" name="gender[]">
+                                </div>
+                              -->
+                                <button class="btn btn-style mt-3">Login </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-------------------------------------------------------- -->
+</body>
+
+</html>
