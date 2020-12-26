b744e9b607d6057f4143580a5935e553e0b17714
Muhammed Shifan <muhammedshifanpazheri@gmail.com>
2020-12-26 15:04:32 +0530

    auto increment constraints is updated

--- Database/hostel_management.sql
+++ Database/hostel_management.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 25, 2020 at 05:23 PM
+-- Generation Time: Dec 26, 2020 at 10:09 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
@@ -31,9 +31,9 @@ CREATE TABLE `administrator` (
   `ha_id` int(10) NOT NULL,
   `f_name` varchar(20) DEFAULT NULL,
   `l_name` varchar(20) DEFAULT NULL,
-  `username` varchar(20) DEFAULT NULL,
-  `mobile` varchar(20) DEFAULT NULL,
-  `password` varchar(20) DEFAULT NULL
+  `username` varchar(20) NOT NULL,
+  `mobile` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -58,7 +58,7 @@ CREATE TABLE `application` (
 
 CREATE TABLE `hostel` (
   `hostel_id` int(10) NOT NULL,
-  `hostel_name` varchar(20) DEFAULT NULL,
+  `hostel_name` varchar(20) NOT NULL,
   `no_of_rooms` int(5) DEFAULT NULL,
   `no_of_students` int(5) DEFAULT NULL,
   `accepted_gen` int(2) DEFAULT NULL
@@ -74,9 +74,9 @@ CREATE TABLE `hostel_manager` (
   `hm_id` int(10) NOT NULL,
   `f_name` varchar(20) DEFAULT NULL,
   `l_name` varchar(20) DEFAULT NULL,
-  `username` varchar(20) DEFAULT NULL,
-  `mobile` varchar(20) DEFAULT NULL,
-  `password` varchar(20) DEFAULT NULL,
+  `username` varchar(20) NOT NULL,
+  `mobile` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
   `admin` int(10) DEFAULT NULL,
   `hostel_id` int(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -133,7 +133,7 @@ CREATE TABLE `student` (
   `l_name` varchar(20) DEFAULT NULL,
   `year` varchar(20) DEFAULT NULL,
   `dept` varchar(50) DEFAULT NULL,
-  `password` varchar(20) DEFAULT NULL,
+  `password` varchar(20) NOT NULL,
   `hostel_id` int(20) DEFAULT NULL,
   `room_id` int(10) DEFAULT NULL,
   `gender` int(2) DEFAULT NULL
@@ -156,15 +156,16 @@ ALTER TABLE `administrator`
 --
 ALTER TABLE `application`
   ADD PRIMARY KEY (`application_no`),
-  ADD KEY `fk_application_student_id` (`student_id`),
+  ADD KEY `fk_application_hm_id` (`hm_id`),
   ADD KEY `fk_application_room_id` (`room_id`),
-  ADD KEY `fk_application_hm_id` (`hm_id`);
+  ADD KEY `fk_application_student_id` (`student_id`);
 
 --
 -- Indexes for table `hostel`
 --
 ALTER TABLE `hostel`
-  ADD PRIMARY KEY (`hostel_id`);
+  ADD PRIMARY KEY (`hostel_id`),
+  ADD UNIQUE KEY `hostel_name` (`hostel_name`);
 
 --
 -- Indexes for table `hostel_manager`
@@ -205,6 +206,46 @@ ALTER TABLE `student`
   ADD PRIMARY KEY (`student_id`),
   ADD KEY `fk_student_hostel_id` (`hostel_id`),
   ADD KEY `fk_student_room_id` (`room_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `administrator`
+--
+ALTER TABLE `administrator`
+  MODIFY `ha_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `application`
+--
+ALTER TABLE `application`
+  MODIFY `application_no` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `hostel`
+--
+ALTER TABLE `hostel`
+  MODIFY `hostel_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `hostel_manager`
+--
+ALTER TABLE `hostel_manager`
+  MODIFY `hm_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `msg_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `room`
+--
+ALTER TABLE `room`
+  MODIFY `room_id` int(10) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
