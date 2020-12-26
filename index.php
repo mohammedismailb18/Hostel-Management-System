9bd21250cfd6a7c6ff487cf3a784d244ee6f24f9
mohammed ismail <mohammedismailck123@gmail.com>
2020-12-26 11:03:40 +0530

    changed the login as hostel manager/admin

--- index.php
+++ index.php
@@ -45,7 +45,8 @@
                                     <input type="password" name="Password" placeholder="Password" required="">
                                 </div>
                                 <button class="btn btn-style mt-3">Login </button>
-                                <p class="already">Login as <a href="#signin">Hostel Manager/Admin</a></p>
+                                <p class="already" style="margin-bottom : -25px; text-align: center;">Login as <a href="#signin">Admin</a></p>
+                                <p class="already">Login as <a href="#signin">Hostel Manager</a></p>
                                 <p class="already" style="margin-top : -1px; text-align: center;">Don't have an account? <a href="#signin">Sign up</a></p>
                             </form>
                         </div>
