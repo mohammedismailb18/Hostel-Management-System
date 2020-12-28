8c27c02b76fb85b1f66acff6c0fbca2b59f54eff
mohammed ismail <mohammedismailck123@gmail.com>
2020-12-28 13:21:21 +0530

    student sign-up page

--- index_signup.php
+++ index_signup.php
@@ -11,36 +11,37 @@
 
     <!-- CSS and Bootsrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="web/css/sign-up.css">
+    <link rel="stylesheet" href="web/css/student-sign-up.css">
 </head>
 
 <body>
-    <div class="bg-opacity">
-        <div class="content container">
 
-        <form action="student_backend/signup.inc.php" method="post" class="signup-form">
-        <div class="row">
+<!-------------- Content --------------->
+<div class="container">
+  
+  <div class="col lg-6 sign-up">
+      <div class="form">
+        <form action="student_backend/signup.inc.php" method="POST" class="register-form">
+          <p class="message-sign-up">Student Sign Up</p>
+          <input type="text" placeholder="Roll No" name="username">
+          <input type="text" placeholder="First Name" name="f_name">
+          <input type="text" placeholder="Last Name" name="l_name">
 
-            <div class="col-md-8">
-            <input type="text" name="username" class="form-control student-text" placeholder="Roll No">
-                <input type="text" name="f_name" class="form-control student-text" placeholder="First Name">
-                <input type="text" name="l_name" class="form-control student-text" placeholder="Last Name">
-
-                <select class="custom-select student-text" style="margin-top: -1px;" name="gender">
+          <select class="custom-select student-text" name="gender">
                     <option selected>Gender</option>
                     <option value="1">Male</option>
                     <option value="0">Female</option>
-                </select>
+          </select>
 
-                <select class="custom-select student-text" name="year" style="margin-top: -1px;">
+          <select class="custom-select student-text" name="year">
                     <option selected>Year of study</option>
                     <option value="1">1st year</option>
                     <option value="2">2nd year</option>
                     <option value="3">3rd year</option>
                     <option value="4">4th year</option>
-                </select>
+            </select> 
 
-                <select class="custom-select student-text" name="dept" style="margin-top: -1px;">
+            <select class="custom-select student-text" name="dept">
                     <option selected>Department</option>
                     <option value="CSE">CSE</option>
                     <option value="ECE">ECE</option>
@@ -48,21 +49,18 @@
                     <option value="ME">ME</option>
                     <option value="EP">EP</option>
                     <option value="CE">CE</option>
-                </select>
-                <input type="text" name="mobile" class="form-control student-text" placeholder="Mobile" style="margin-top: -1px;">
-                <input type="password" name="password" class="form-control student-text" placeholder="Password">
-                <input type="password" name="confpassword" class="form-control student-text" placeholder=" Re-enter Password">
-            </div>
-            <div class="col-md-10" style="padding-left: 540px;">
-            <button type="submit" name="signup-student"  class="btn-lg btn-primary" style="margin-top:10px;">Submit</button>
-            </div>
-        </div>
-    </form>
+             </select>
 
-        </div>
-    </div>
-    <!-------------- Login Form ----------------------------------------------->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+          <input type="text" placeholder="Mobile" name="mobile">
+          <input type="password" placeholder="Password" name="password">
+          <input type="password" placeholder="Re-Enter Password" name="confpassword">
+          <button type="submit" name="signup-student">Sign Up</button>
+          <p class="message">Already registered? <a href="index.php">Sign In</a></p>
+        </form>
+  </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
